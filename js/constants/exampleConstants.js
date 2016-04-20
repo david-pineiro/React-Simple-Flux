401a96ab9d3270b0484ebc7d566b2b1602e5a817
david-pineiro <david.pineiro@logtrust.com>
2016-04-20 12:16:58 +0200

    Added files via upload

---
+++ js/constants/exampleConstants.js
@@ -0,0 +1,10 @@
+var keyMirror = require('fbjs/lib/keyMirror');
+
+// Define action constants
+module.exports = keyMirror({
+    GET_SELECT_DATA: null,
+    GET_DATA_DESCRIPTION: null,
+    SET_SELECTED_COSA: null,
+    SAVE_DESC_ON_STORE: null,
+    SET_USE_STYLE: null,
+});
